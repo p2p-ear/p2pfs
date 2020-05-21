d5ef82de8b0b64760d1286d9c283f53bd7402406
ilykuleshov <ilykuleshov@gmail.com>
2020-05-21 13:33:04 +0300

    Added variable-sized cypher keys

--- client/libs/include/cypher.h
+++ client/libs/include/cypher.h
@@ -1,15 +1,21 @@
 class XORcypher {
 private:
-    char secret_[4096];
+    char* secret_;
+    const unsigned int size_;
 public:
     // Initialize secret with random bytes
-    XORcypher();
+    XORcypher(unsigned int size);
 
     // Initialize secret with given bytes
-    XORcypher(char* secret);
+    XORcypher(unsigned int size, char* secret);
+
+    ~XORcypher();
 
     // Retrieve secret
     const char* get_secret();
+
+    // Retrieve secret size
+    const int get_size();
 
     // Cypher shard in-place
     void operator()(char* shard);
--- client/libs/src/cypher.cpp
+++ client/libs/src/cypher.cpp
@@ -2,23 +2,31 @@
 #include <random>
 #include <cstring>
 
-XORcypher::XORcypher() {
+XORcypher::XORcypher(unsigned int size) : size_(size), secret_(new char[size]) {
     std::independent_bits_engine<std::default_random_engine, __CHAR_BIT__, char> engine;
-    for (unsigned int i = 0; i < 4096; i++) {
+    for (unsigned int i = 0; i < size_; i++) {
         secret_[i] = engine();
     }
 }
 
-XORcypher::XORcypher(char* secret) {
-    std::memcpy(secret_, secret, 4096);
+XORcypher::XORcypher(unsigned int size, char* secret) : size_(size), secret_(new char[size]) {
+    std::memcpy(secret_, secret, size_);
+}
+
+XORcypher::~XORcypher() {
+    delete[] secret_;
 }
 
 const char* XORcypher::get_secret() {
     return secret_;
 }
 
+const int XORcypher::get_size() {
+    return size_;
+}
+
 void XORcypher::operator()(char* shard) {
-    for (unsigned int i = 0; i < 4096; i++) {
+    for (unsigned int i = 0; i < size_; i++) {
         shard[i] ^= secret_[i];
     }
 }
