8b7614c9208a9860dca9170ccaedb0841e9c0093
VashchilkoAV <vashchilko.av@phystech.edu>
2020-05-22 00:18:55 +0300

    compilation stage 2: download

--- client/libs/include/duload.h
+++ client/libs/include/duload.h
@@ -44,8 +44,8 @@ std::string Pbzip2(const std::string& filename);
 
 
 //download
-int download(const std::string& filename, const std::string& path, visFuncs*, int method);
-int Merge(std::vector<std::string>& shards, const std::string& filename, const std::string& path, visFuncs*);
+int download(const std::string& filename, const std::string& path, visFuncs* vis, int method, std::string ip, unsigned long ringsz, std::string JWT, unsigned long nshards, std::string suff, unsigned long size);
+int Merge(std::vector<std::string>& shards, const std::string& filename, const std::string& path, visFuncs* vis, std::string ip, unsigned long ringsz, std::string JWT, std::string suff, unsigned long size);
 int unZIPFunc(const std::string& filename, const std::string& path, int method, visFuncs*);
 
 //unzip funcs
--- client/libs/include/duload_export.h
+++ client/libs/include/duload_export.h
@@ -12,7 +12,7 @@ typedef struct visFuncs {
     void (*End2) (const std::string&);
 } visFuncs;
 
-extern int download(const std::string& filename, const std::string& path, visFuncs*, int method);
+extern int download(const std::string& filename, const std::string& path, visFuncs* vis, int method, std::string ip, unsigned long ringsz, std::string JWT, unsigned long nshards, std::string suff, unsigned long size);
 extern int UploadFile(const std::string& filename, const std::string& suff, bool remove, visFuncs* vis, unsigned long long shardLength, int method, std::string ip, unsigned long ringsz, std::string JWT);
-extern int Merge(std::vector<std::string>& shards, const std::string& filename, const std::string& path, visFuncs*);
+extern int Merge(std::vector<std::string>& shards, const std::string& filename, const std::string& path, visFuncs* vis, std::string ip, unsigned long ringsz, std::string JWT, std::string suff, unsigned long size);
 extern int unZIPFunc(const std::string& filename, const std::string& path, int method, visFuncs*);
