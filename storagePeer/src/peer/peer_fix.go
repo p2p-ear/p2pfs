9ed033c765baffe95b7959afc457b94f889c2a50
ilykuleshov <ilykuleshov@gmail.com>
2020-05-22 11:04:11 +0300

    Added fix

--- storagePeer/src/peer/peer_fix.go
+++ storagePeer/src/peer/peer_fix.go
@@ -1,34 +1,46 @@
 package peer
 
 import (
-  "fmt"
-  "bytes"
-  "net/http"
+	"bytes"
+	"fmt"
+	"net/http"
 )
 
-func (p *Peer) fixRoutine() {
+func (p *Peer) fixRoutine() error {
+	const MAX_FILESIZE = 4096 * 1600 / 8
+	filecont := make([]byte, MAX_FILESIZE)
 
-  for {
-    newFile := <- p.ring.NewFilesChannel
-    fmt.Printf("Have to download %s", newFile)
-  }
+	for {
+		newFile := <-p.ring.NewFilesChannel
+		readCertificate := "miha.day.sertificat.pochitat"
+		empty, err := DownloadFileRSC(p.ring.self.IP, newFile, p.ring.maxnodes, filecont, certificate)
+		if err != nil {
+			return err
+		}
+
+		writeCertificate := "miha.day.sertificat.popisat"
+		err = UploadFileRSC(p.ring.self.IP, newFile, p.ring.maxnodes, filecont[:MAX_FILESIZE-empty], certificate)
+		if err != nil {
+			return err
+		}
+	}
 }
 
 const SERVER_IP = "http://172.104.136.183"
 
 func (p *Peer) notifyAboutArrival() {
 
-  notifyUrl := fmt.Sprintf("%s/auth/node/add", SERVER_IP)
+	notifyUrl := fmt.Sprintf("%s/auth/node/add", SERVER_IP)
 
-  var jsonStr = []byte(fmt.Sprintf(`{"ip_address":"%s"}`, p.ownIP))
-  req, err := http.NewRequest("POST", notifyUrl, bytes.NewBuffer(jsonStr))
-  req.Header.Set("Content-Type", "application/json")
+	var jsonStr = []byte(fmt.Sprintf(`{"ip_address":"%s"}`, p.ownIP))
+	req, err := http.NewRequest("POST", notifyUrl, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
 
-  client := &http.Client{}
-  resp, err := client.Do(req)
-  if err != nil {
-      panic(err)
-  }
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
 
-  resp.Body.Close()
+	resp.Body.Close()
 }
