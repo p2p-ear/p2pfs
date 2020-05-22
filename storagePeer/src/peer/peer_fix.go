e40da367f75bbea752764747a5f351baf64824f2
TheRussianFella <maximmanainen@gmail.com>
2020-05-22 10:43:58 +0300

    fix

--- storagePeer/src/peer/peer_fix.go
+++ storagePeer/src/peer/peer_fix.go
@@ -16,11 +16,11 @@ func (p *Peer) fixRoutine() {
 
 const SERVER_IP = "http://172.104.136.183"
 
-func (n *RingNode) notifyAboutArrival() {
+func (p *Peer) notifyAboutArrival() {
 
   notifyUrl := fmt.Sprintf("%s/auth/node/add", SERVER_IP)
 
-  var jsonStr = []byte(fmt.Sprintf(`{"ip_address":"%s"}`, n.self.IP))
+  var jsonStr = []byte(fmt.Sprintf(`{"ip_address":"%s"}`, p.ownIP))
   req, err := http.NewRequest("POST", notifyUrl, bytes.NewBuffer(jsonStr))
   req.Header.Set("Content-Type", "application/json")
 
