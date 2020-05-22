ac0c15f8055a58a668c110142afcfae32456db55
ilykuleshov <ilykuleshov@gmail.com>
2020-05-22 13:09:55 +0300

    а

--- storagePeer/src/peer/peer_certificates.go
+++ storagePeer/src/peer/peer_certificates.go
@@ -21,7 +21,13 @@ type FileClaim struct {
 	//Action - 0=read; 1=write; 2=delete
 	Act int8 `json:"act"`
 
-	jwt.StandardClaims
+	Audience  string `json:"aud,omitempty"`
+	ExpiresAt int64  `json:"exp,omitempty"`
+	Id        string `json:"jti,omitempty"`
+	IssuedAt  int64  `json:"iat,omitempty"`
+	Issuer    string `json:"iss,omitempty"`
+	NotBefore int64  `json:"nbf,omitempty"`
+	Subject   int    `json:"sub,omitempty"`
 }
 
 const (
