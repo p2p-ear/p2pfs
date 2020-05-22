2f60f073e298faf5c14b5761e983e0a9bc9e5d93
TheRussianFella <maximmanainen@gmail.com>
2020-05-22 10:37:47 +0300

    Server com

---
+++ storagePeer/src/peer/peer_fix.go
@@ -0,0 +1,34 @@
+package peer
+
+import (
+  "fmt"
+  "bytes"
+  "net/http"
+)
+
+func (p *Peer) fixRoutine() {
+
+  for {
+    newFile := <- p.ring.NewFilesChannel
+    fmt.Printf("Have to download %s", newFile)
+  }
+}
+
+const SERVER_IP = "http://172.104.136.183"
+
+func (n *RingNode) notifyAboutArrival() {
+
+  notifyUrl := fmt.Sprintf("%s/auth/node/add", SERVER_IP)
+
+  var jsonStr = []byte(fmt.Sprintf(`{"ip_address":"%s"}`, n.self.IP))
+  req, err := http.NewRequest("POST", notifyUrl, bytes.NewBuffer(jsonStr))
+  req.Header.Set("Content-Type", "application/json")
+
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+      panic(err)
+  }
+
+  resp.Body.Close()
+}
